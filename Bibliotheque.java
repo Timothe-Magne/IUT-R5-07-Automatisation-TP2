40eac00d0fbf76194ba70b2184fbb2959fcac2da
CelimS <166292533+CelimS@users.noreply.github.com>
2026-09-16 14:17:15 +0200

    Changement du texte

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -2,6 +2,6 @@ public class Bibliotheque
 {
     public static void main(String[] arg)
     {
-        System.out.println("Bibliotheque");
+        System.out.println("Bibliotheque completement vide et inutile");
     }
 }
